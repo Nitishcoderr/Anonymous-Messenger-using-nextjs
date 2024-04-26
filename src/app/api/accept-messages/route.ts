68d04b6e5582656632cb66709711abfce2ef0cad
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2024-04-26 23:54:22 +0530

    nextauth added

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,13 @@
+// import {getServerSession} from "next-auth"
+// import { authOptions } from "../[...nextauth]/option"
+// import dbConnect from "@/lib/dbConnect"
+// import UserModel from "@/model/User"
+
+// import {User} from 'next-auth'
+
+// export async function POST(request:Request){
+//     await dbConnect()
+
+//     const session = await getServerSession(authOptions)
+//     session?.user
+// }
